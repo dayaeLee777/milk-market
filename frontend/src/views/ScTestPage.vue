796999d70f7e1ace9946216523c9dea3daa8c1bd
이상우 <okqwaszx12@naver.com>
2022-03-14 16:18:46 +0900

    sc-setting complete

--- frontend/src/views/ScTestPage.vue
+++ frontend/src/views/ScTestPage.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <div>
-      <button @click="testMeta">클릭</button>
-      <button @click="testAcc">확인용</button>
-    </div>
     <div class="d-flex-row">
       <p>지갑 주소 {{ tx.walletAdress }}</p>
       <p>지갑 생성</p>
@@ -12,9 +8,26 @@
     </div>
 
     <div>
-      <p>잔액 확인</p>
-      <span>{{ balance }}ETH</span>
-      <button @click="checkBalance">잔액 확인</button>
+      <p>Coinbase 잔액 확인</p>
+      <div>
+      <span>{{ ethBalance }}ETH</span>
+      <button @click="checkBalance">ETH 잔액 확인</button>
+      </div>
+      <div>
+        <span>{{ milkBalance }}MILK</span>
+        <button @click="checkMilk">MILK 잔액 확인</button>
+      </div>
+    </div>
+
+    <div>
+      <p>MILK 전송</p>
+      <p>From</p>
+      <input type="text" v-model="from">
+      <p>To</p>
+      <input type="text" v-model="to">
+      <p>전송 MILK</p>
+      <input type="text" v-model="amount">
+      <button @click="sendMilk">MILK 전송</button>
     </div>
   </div>
 
@@ -29,30 +42,32 @@ export default {
   data() {
     return {
       web3: "",
+      coinbaseAdress: "",
       contractAdress: "",
       MilkToken: MilkToken,
+      from: "",
+      to: "",
+      amount: 0,
       tx: {
         password: "",
         walletAdress: [],
       },
-      balance: 0,
+      ethBalance: 0,
+      milkBalance: 0,
+      contract: "",
     }
   },
   methods: {
     // 메타마스크 설치 후 프로바이더 사용
-    testMeta() {   
+    makeContract() {   
       const Web3 = require('web3');
       const web3 = new Web3(new Web3.providers.HttpProvider(ENDPOINT));
       const ENDPOINT = 'http://localhost:8545';      
 
-      let contract = web3.eth.contract(MilkToken.abi).at(this.contractAdress)
 
-      contract.balanceOf(this.tx.walletAdress[2], res => {
-        console.log(res)
-      })
-    },
-    testAcc() {
-  
+      let contract =  new web3.eth.Contract(MilkToken.abi, this.contractAdress);
+      this.contract = contract
+      
     },
   	createWallet() {
       const Web3 = require('web3');
@@ -73,20 +88,47 @@ export default {
       const Web3 = require('web3');
       const web3 = new Web3(new Web3.providers.HttpProvider(ENDPOINT));
       const ENDPOINT = 'http://localhost:8545';      
-      web3.eth.getBalance(this.tx.walletAdress[6]).then(res => {
-        this.balance = res / 10**18
+      web3.eth.getBalance(this.coinbaseAdress).then(res => {
+        this.ethBalance = (res / 10**18).toLocaleString();
       })
+    },
+    // 1000MILK = 1ETH (1MILK = 10*15 wei)
+    async checkMilk() {
+      const milkBalance = await this.contract.methods.balanceOf(this.coinbaseAdress).call();
+      this.milkBalance = (milkBalance / 10**15).toLocaleString();
+      const result = await this.contract.methods.approve(this.coinbaseAdress, milkBalance).call();
+    },
+    async sendMilk() {
+      const Web3 = require('web3');
+      const web3 = new Web3(new Web3.providers.HttpProvider(ENDPOINT));
+      const ENDPOINT = 'http://localhost:8545';       
+      const from = this.from;
+      const to = this.to;
+      const amount = Number(this.amount);
+      const is_allowed = await this.contract.methods.allowance(to, amount).call();
+      console.log(is_allowed)
+      // let result;
+      // if (is_allowed) {
+      //   console.log(is_allowed)
+      //   result = await this.contract.methods.transferFrom(fromAddress, toAddress, amount).call();
+      // }
+      // console.log(result)
+      this.from = "";
+      this.to = "";
+      this.amount = 0;
     }
   },
   mounted() {
-    const contractAdress = "0xb3112448189B3d4737DABD06Ed2e574Ed51A9174"
+    const contractAdress = "0x36Bb0950E8c2B909bB9AD77573b5fF3982c98Bb8"
     this.contractAdress = contractAdress 
     const Web3 = require('web3');
     const web3 = new Web3(new Web3.providers.HttpProvider(ENDPOINT));
     const ENDPOINT = 'http://localhost:8545';      
     web3.eth.getAccounts().then(res => {
       this.tx.walletAdress = res
-    })
+      this.coinbaseAdress = res[0]
+    });
+    this.makeContract();
   }
 }
 </script>
