37888588c15ac045853da201b35887f1d0dcccde
sahngrhee <87463462+sahngrhee@users.noreply.github.com>
2022-03-21 14:18:04 +0900

    feature/product

---
+++ frontend/src/components/Product.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <img
+      src=""
+      class="card-img-top"
+      alt=""
+    >
+    <div class="card-body">
+      <h5 class="card-title">{{ content.product_name }}</h5>
+      <p class="card-text">{{ content.product_introduce }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: Object,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/ProductList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div
+      v-for="(content, index) in contents"
+      :key="index"
+    >
+      <product :content="content"></product>
+    </div>
+  </div>
+</template>
+
+<script>
+import Product from '@/components/Product';
+export default {
+  components: {
+    Product
+  },
+  data: () => ({
+    contents: [{ product_name: "사과", product_introduce: '맛있는' }, { product_name: "사과", product_introduce: '맛있는' }, { product_name: "사과", product_introduce: '맛있는' }]
+  })
+}
+</script>
+
+<style>
+</style>
