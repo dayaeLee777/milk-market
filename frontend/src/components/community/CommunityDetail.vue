4de44c46447124d8fdaab823acca4c9a14ec42cf
yunhy96 <yunhy96@naver.com>
2022-03-24 13:59:55 +0900

    board

--- frontend/src/components/community/CommunityDetail.vue
+++ frontend/src/components/community/CommunityDetail.vue
@@ -8,7 +8,7 @@
       class="container"
     >
 
-    <!-- <a>{{$router.query.coId}}</a> -->
+    <!-- <a>{{$route.params.coId}}</a> -->
 
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">글 제목</label>
@@ -19,7 +19,7 @@
         <textarea class="form-control" v-model="content" id="exampleFormControlTextarea1" rows="10" placeholder="글 내용을 입력해주세요"></textarea>
       </div>
 
-    <button type="button" class="btn btn-primary" style="justify-content : center" >
+    <button type="button" class="btn btn-primary" @click="modifyCommunity" style="justify-content : center" >
       글 수정 버튼
     </button>
 
@@ -46,14 +46,14 @@ export default {
     getCommunityDetail(){
       const token = this.$store.state.user.JWTToken;
       console.log(token+"community 세부정보를 조회할때 백엔드로 보내는 토큰입니다.");
-      console.log(this.$router.query.coId);
+      console.log(this.$route.params.coId);
 
       const headers = {
         Authorization: `Bearer ${token}`
       }
 
       axios({
-        url : `http://localhost:8080/api/community/${this.$router.query.coId}`,
+        url : `http://localhost:8080/api/community/${this.$route.params.coId}`,
         method: 'get',
         headers,
       })
@@ -65,9 +65,40 @@ export default {
       .catch((err)=>{
         console.log(err);
         console.log("글 조회 실패");
-      })
+      })      
+    },
+    modifyCommunity(){
+      const token = this.$store.state.user.JWTToken;
+      console.log(token+"community 세부정보 수정하기 버튼을 누르면 나오는 토큰입니다.");
 
-      
+      const headers = {
+        Authorization: `Bearer ${token}`
+      }
+
+      const data = {
+        communityId : this.$route.params.coId,
+        title : this.title,
+        content : this.content,
+      }
+
+      axios({
+        url : `http://localhost:8080/api/community`,
+        method: 'put',
+        headers,
+        data,
+      })
+      .then((res)=>{
+        console.log("글 수정 성공");
+        alert("글 수정 성공")
+        this.$router.push("/board")
+        // this.title = res.data.title;
+        // this.content = res.data.content;
+      })
+      .catch((err)=>{
+        alert("글 수정 실패")
+        console.log(err);
+        console.log("글 수정 실패");
+      }) 
     }
   },
 };
