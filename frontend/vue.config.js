55916af8fe91b1b2d001b10a9ecfaaf449fa9048
DayeLEE <ldy1853@naver.com>
2022-03-25 17:47:28 +0900

    chore: update dockerfile for nginx

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   devServer: {
     https: false,
-    port:8083,
+    port:3000,
     open:true,
     // proxy:{
     //   '/api' : {
@@ -9,6 +9,7 @@ module.exports = {
     //   }
     // },
     historyApiFallback: true,
+    disableHostCheck: true,
     hot:true
   },
   css: {
@@ -19,5 +20,5 @@ module.exports = {
     'vuetify'
   ],
   lintOnSave:false,
-  // outputDir: '../backend/src/main/resources/dist' //빌드 시 dist 파일 생성 경로 지정
+  outputDir: '../backend/src/main/resources/dist' //빌드 시 dist 파일 생성 경로 지정
 };
