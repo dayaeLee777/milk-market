1e0136290893cd38d2c31694dbd1d9172e33d62d
권도혁 <fsa3806@naver.com>
2022-03-29 11:00:22 +0900

    refactor: change user detail

--- frontend/src/components/item/ItemEach.vue
+++ frontend/src/components/item/ItemEach.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="card">
-    <img
-      :src="content.item_img"
-      class="card-img-top"
-      alt=""
-    >
+  <div
+    class="card col-sm-12 col-lg-6"
+    @click="itemDetail(content.itemId)"
+    style="cursor: pointer"
+  >
+    <img v-if="!key" :src="item_img" class="card-img-top" alt="사진없음" />
+    <img v-else :src="content.files[key]" class="card-img-top" alt="" />
     <div class="card-body">
-      <h5 class="card-title">{{ content.item_name }}</h5>
-      <p class="card-text">{{ content.item_introduce }}</p>
+      <div>
+        <h5 class="card-title">{{ content.itemName }}</h5>
+        <hr />
+        <p class="card-text" id="fix">{{ content.description }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -16,9 +20,34 @@
 export default {
   props: {
     content: Object,
-  }
-}
+  },
+  data() {
+    return {
+      key: "",
+      item_img:
+        "https://3.bp.blogspot.com/-ZKBbW7TmQD4/U6P_DTbE2MI/AAAAAAAADjg/wdhBRyLv5e8/s1600/noimg.gif",
+    };
+  },
+  mounted() {
+    this.key = Object.keys(this.content.files)[0];
+    console.log("key 출력" + this.key);
+  },
+  methods: {
+    itemDetail(itemId) {
+      this.$router.push({
+        name: "item.detail",
+        params: { id: this.content.itemId },
+      });
+      console.log("경로 : " + this.content.files[this.key]);
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+img.card-img-top {
+  width: 286px;
+  height: 150px;
+  object-fit: cover;
+}
 </style>
--- frontend/src/components/item/ItemList.vue
+++ frontend/src/components/item/ItemList.vue
@@ -1,14 +1,7 @@
 <template>
-  <div
-    id="main-overview"
-    class="container"
-  >
+  <div id="main-overview" class="container">
     <div class="row row-cols-2 row-cols-md-4 g-5">
-      <div
-        class="col"
-        v-for="(content, index) in contents"
-        :key="index"
-      >
+      <div class="col" v-for="(content, index) in contents" :key="index">
         <item-each :content="content"></item-each>
       </div>
     </div>
@@ -16,25 +9,52 @@
 </template>
 
 <script>
-import axios from 'axios'
-import ItemEach from '@/components/item/ItemEach';
+import axios from "axios";
+import ItemEach from "@/components/item/ItemEach";
 
 export default {
-
   components: {
-    ItemEach
+    ItemEach,
   },
   data: () => ({
     contents: [
-      { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
-      { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
-      { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
-      { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
-      { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
-      { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
-    ]
-  })
-}
+      // { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
+      // { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
+      // { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
+      // { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
+      // { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
+      // { item_img: 'https://www.ktong.kr/wp-content/uploads/2020/08/%EC%82%AC%EA%B3%BC-%EB%8B%A4%EC%9D%B4%EC%96%B4%ED%8A%B8-5%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EC%A0%81%EC%9D%84-%EA%B2%BD%ED%97%98%ED%95%98%EC%84%B8%EC%9A%94.jpg', item_name: '사과', item_introduce: '맛있는 사과 팔아요' },
+    ],
+  }),
+  mounted() {
+    this.getItemList();
+    console.log("마운트 되자마자 보여주는 콘솔");
+  },
+  methods: {
+    getItemList() {
+      const token = this.$store.state.user.JWTToken;
+      console.log("아이템 리스트 가져오기" + token);
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      axios({
+        url: `http://localhost:8080/api/item/list/`,
+        method: "get",
+        headers,
+      })
+        .then((res) => {
+          console.log(res);
+          this.contents = res.data.list;
+          console.log(" 아이템 리스트 로드 성공");
+          console.log(res);
+          console.log(this.contents);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
 <style>
