60a057f09f12d0f6df72ec762b93da3122182009
이상우 <okqwaszx12@naver.com>
2022-03-31 13:48:23 +0900

    merge

--- frontend/src/api/item.js
+++ frontend/src/api/item.js
@@ -1,8 +1,7 @@
 import store from "../store/index.js";
 import { createInstance } from "./index.js";
 
-// const token = store.getters.getJWTToken;
-const token = store.state.user.JWTToken
+const token = store.getters.getJWTToken;
 const instance = createInstance();
 // function signup(email, nickName, password, success, fail) {
 //   const user = {
@@ -16,10 +15,10 @@ const instance = createInstance();
 //     .then(success)
 //     .catch(fail);
 // }
-function findAll (success, fail) {
+function findAll(success, fail) {
   instance.get("/api/item/").then(success).catch(fail);
 }
-function getItemList (bcode, sortBy, order, page, size, success, fail) {
+function getItemList(bcode, sortBy, order, page, size, success, fail) {
   const ItemList = {
     bcode: bcode,
     sortBy: sortBy,
@@ -55,6 +54,13 @@ function getSearchItem(
     size: size,
   };
 
+  instance
+    .post("/api/item/search", JSON.stringify(ItemList), {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then(success)
+    .catch(fail);
+}
 
 function findItemListByPage(pageNumber, success, fail) {
   const ItemList = {
@@ -70,14 +76,14 @@ function findItemListByPage(pageNumber, success, fail) {
     .catch(fail);
 }
 
-function findItemsByOwner (userId, success, fail) {
+function findItemsByOwner(userId, success, fail) {
   instance
     .get("/api/item/seller/" + userId)
     .then(success)
     .catch(fail);
 }
 
-function findById (itemId, success, fail) {
+function findById(itemId, success, fail) {
   instance
     .get("/api/item/" + itemId, {
       headers: { Authorization: `Bearer ${token}` },
@@ -86,7 +92,7 @@ function findById (itemId, success, fail) {
     .catch(fail);
 }
 
-function findHistoryById (itemId, success, fail) {
+function findHistoryById(itemId, success, fail) {
   instance
     .get("/api/item/history/" + itemId)
     .then(function (response) {
@@ -95,28 +101,28 @@ function findHistoryById (itemId, success, fail) {
     .catch(fail);
 }
 
-function findBySeller (id, success, fail) {
+function findBySeller(id, success, fail) {
   instance
     .get("/api/item/seller/" + id)
     .then(success)
     .catch(fail);
 }
 // 구매 완료한 상품들 가져오기
-function findByBuyer (id, success, fail) {
+function findByBuyer(id, success, fail) {
   instance
     .get("/api/item/buyer/" + id)
     .then(success)
     .catch(fail);
 }
 // 입찰 중인 상품들 가져오기
-function findByBidder (id, success, fail) {
+function findByBidder(id, success, fail) {
   instance
     .get("/api/item/bidder/" + id)
     .then(success)
     .catch(fail);
 }
 
-function create (body, success, fail, final) {
+function create(body, success, fail, final) {
   instance
     .post("/api/item", JSON.stringify(body))
     .then(success)
@@ -124,11 +130,11 @@ function create (body, success, fail, final) {
     .finally(final);
 }
 
-function update (body, success, fail) {
+function update(body, success, fail) {
   instance.put("/api/item", JSON.stringify(body)).then(success).catch(fail);
 }
 
-function remove (id, success, fail) {
+function remove(id, success, fail) {
   instance
     .delete("/api/item/" + id)
     .then(success)
@@ -136,14 +142,14 @@ function remove (id, success, fail) {
 }
 
 // 구매자가 배송중인 상품을 구매 확정
-function confirm (itemId, buyer, success, fail) {
+function confirm(itemId, buyer, success, fail) {
   instance
     .put("/api/item/" + itemId + "/by/" + buyer)
     .then(success)
     .catch(fail);
 }
 
-function findMySaleItems (userId, success, fail) {
+function findMySaleItems(userId, success, fail) {
   instance
     .get("api/item/of/" + userId)
     .then((res) => success(res))
