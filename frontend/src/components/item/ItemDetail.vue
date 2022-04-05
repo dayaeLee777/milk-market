9c72525bd9a24fb9da6bc08c4ae8534ba084a2ec
권도혁 <fsa3806@naver.com>
2022-04-05 10:15:31 +0900

    feature:fe-category-click

--- frontend/src/components/item/ItemDetail.vue
+++ frontend/src/components/item/ItemDetail.vue
@@ -1,26 +1,30 @@
 <template>
-  <div>   
+  <div>
     <div class="container mt-5">
       <div class="row">
         <div class="col-md-8 mx-auto">
-          <div class="card" id="item-detail"> 
+          <div class="card" id="item-detail">
             <div class="card-body">
               <div class="form-group">
                 <h3>
                   <!--search 완성 되면 실행해보기-->
-                  <a href="">{{ item.category }}</a> >
+                  <router-link
+                    :to="{
+                      name: 'shop',
+                      query: { category: `${item.category}` },
+                    }"
+                    >{{ item.category }}</router-link
+                  >
+                  >
                   {{ item.itemName }}
                 </h3>
               </div>
-              <div
-                v-for="imgName in item.keys"
-                :key="imgName.id"
-              >
+              <div v-for="imgName in item.keys" :key="imgName.id">
                 <img
                   class="center"
                   :src="getImg(item.files[imgName])"
                   id="item-detail-img"
-                  style="max-width: 100%;"
+                  style="max-width: 100%"
                 />
               </div>
               <!--v-for-->
@@ -28,10 +32,7 @@
                 <h4 class="alert alert-primary">{{ item.price }} MILK</h4>
               </div>
               <div class="form-group">
-                <label
-                  id="user"
-                  class="text-secondary"
-                >판매자</label>
+                <label id="user" class="text-secondary">판매자</label>
                 <p>
                   {{ item.userNickname }}
                   <!-- ({{ item.seller.email }}) -->
@@ -42,10 +43,7 @@
                 <p>{{ item.regDate }}</p>
               </div>
               <div class="form-group">
-                <label
-                  id="explanation"
-                  class="text-secondary"
-                >상품 설명</label>
+                <label id="explanation" class="text-secondary">상품 설명</label>
                 <p v-if="item.description.length > 0">{{ item.description }}</p>
                 <p v-else>-</p>
               </div>
@@ -56,59 +54,101 @@
                 v-if="userId !== item.userId"
               >
                 <div>
-                  <button
-                    @click="goChatting"
-                    class="btn btn-lg btn-primary"
-                  >채팅하기</button>
+                  <button @click="goChatting" class="btn btn-lg btn-primary">
+                    채팅하기
+                  </button>
                 </div>
                 <div v-if="item.division === 'A01'">
-                  <button class="btn btn-lg btn-primary"
-                     data-bs-toggle="modal" data-bs-target="#rentModal">
-                     대여하기
-                     </button>
+                  <button
+                    class="btn btn-lg btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#rentModal"
+                  >
+                    대여하기
+                  </button>
                 </div>
                 <div v-else>
-                  <button class="btn btn-lg btn-primary" 
-                    data-bs-toggle="modal" data-bs-target="#purchaseModal">
+                  <button
+                    class="btn btn-lg btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#purchaseModal"
+                  >
                     구매하기
-                    </button>
+                  </button>
                 </div>
-                <div class="modal fade" id="rentModal" tabindex="-1" aria-labelledby="rentModalLabel" aria-hidden="true">
+                <div
+                  class="modal fade"
+                  id="rentModal"
+                  tabindex="-1"
+                  aria-labelledby="rentModalLabel"
+                  aria-hidden="true"
+                >
                   <div class="modal-dialog">
                     <div class="modal-content">
                       <div class="modal-header">
-                        <h5 class="modal-title" id="rentModalLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h5 class="modal-title" id="rentModalLabel">
+                          Modal title
+                        </h5>
+                        <button
+                          type="button"
+                          class="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
                       </div>
-                      <div class="modal-body">
-                        ...
-                      </div>
+                      <div class="modal-body">...</div>
                       <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button
+                          type="button"
+                          class="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button type="button" class="btn btn-primary">
+                          Save changes
+                        </button>
                       </div>
                     </div>
                   </div>
                 </div>
 
-                <div class="modal fade" id="purchaseModal" tabindex="-1" aria-labelledby="purchaseModalLabel" aria-hidden="true">
+                <div
+                  class="modal fade"
+                  id="purchaseModal"
+                  tabindex="-1"
+                  aria-labelledby="purchaseModalLabel"
+                  aria-hidden="true"
+                >
                   <div class="modal-dialog">
                     <div class="modal-content">
                       <div class="modal-header">
-                        <h5 class="modal-title" id="purchaseModalLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h5 class="modal-title" id="purchaseModalLabel">
+                          Modal title
+                        </h5>
+                        <button
+                          type="button"
+                          class="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
                       </div>
-                      <div class="modal-body">
-                        ...
-                      </div>
+                      <div class="modal-body">...</div>
                       <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button
+                          type="button"
+                          class="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button type="button" class="btn btn-primary">
+                          Save changes
+                        </button>
                       </div>
                     </div>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
@@ -121,12 +161,16 @@
 <script>
 import { Code } from "@/utils/enum.js";
 import { findById } from "@/api/item.js";
-import { API_BASE_URL, BLOCKCHAIN_URL, CASH_CONTRACT_ADDRESS } from "@/config/index.js";
+import {
+  API_BASE_URL,
+  BLOCKCHAIN_URL,
+  CASH_CONTRACT_ADDRESS,
+} from "@/config/index.js";
 
 const vm = this;
 export default {
   name: "ItemDetail",
-  data () {
+  data() {
     return {
       item: {
         id: "",
@@ -149,23 +193,33 @@ export default {
     };
   },
   methods: {
-    getImg (name) {
+    getImg(name) {
       if (name) {
         return name;
       }
       return null;
     },
-    doPay() {
-      
-    },
-    goChatting () {
-      console.log("채팅방으로 가는 버튼을 눌럿음")
-      const A = this.item.userNickname > this.$store.state.user.userNickname ? this.$store.state.user.userNickname : this.item.userNickname;
-      const B = this.item.userNickname > this.$store.state.user.userNickname ? this.item.userNickname : this.$store.state.user.userNickname;
-      this.$router.push({ name: "room", params: { userNickname: this.item.userNickname, sessionId: A + '1' + B } });
+    doPay() {},
+    goChatting() {
+      console.log("채팅방으로 가는 버튼을 눌럿음");
+      const A =
+        this.item.userNickname > this.$store.state.user.userNickname
+          ? this.$store.state.user.userNickname
+          : this.item.userNickname;
+      const B =
+        this.item.userNickname > this.$store.state.user.userNickname
+          ? this.item.userNickname
+          : this.$store.state.user.userNickname;
+      this.$router.push({
+        name: "room",
+        params: {
+          userNickname: this.item.userNickname,
+          sessionId: A + "1" + B,
+        },
+      });
     },
   },
-  created () {
+  created() {
     console.log("created");
     this.item.id = this.$route.params.id;
     console.log(this.item.id);
